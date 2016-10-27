ec492cbb5bed72facd04a03e6a5f8b65771beb86
pidewap <pidewap@gmail.com>
2016-10-27 13:02:15 +0700

    Create head.php

---
+++ head.php
@@ -0,0 +1,31 @@
+<div class="clearfix"></div> 
+<div class="navbar navbar-default navbar-fixed-top" role="navigation" id="slide-nav">
+ <div class="container">
+ <div class="navbar-header" style="left: 0px;">
+<a class="navbar-toggle"> 
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+     </a> 
+<a href="/" class="navbar-brand"><span class="hidden-xs glyphicon glyphicon-play"></span> <span class="hidden-lg header-title">ShareLagu.Info</span></a>
+</div>
+ <div id="slidemenu" class="" style="left: -100%;">
+<form class="navbar-form navbar-left" role="search" action="/site_fullsearch.xhtml" method="get">
+					<div class="input-group">
+						<input type="text" class="form-control" placeholder="Music Search..." name="get-q">
+						<span class="input-group-btn">
+							<button type="submit" class="btn btn-default">
+								<span class="glyphicon glyphicon-search">
+									<span class="sr-only">Search</span>
+								</span>
+							</button>
+						</span>
+					</div>
+				</form>
+
+ <ul class="nav navbar-nav navbar-right"><li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Account <span class="caret"></span></a> <ul class="dropdown-menu"> <li><a href="/login_site.xhtml?do_id=119"> Login</a></li> <li><a href="/registration_site.xhtml?do_id=119">Sign Up</a></li> </ul> </li></ul>
+<ul class="nav navbar-nav navbar-left"> <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Browse Music <b class="caret"></b></a> <ul class="dropdown-menu"> <li><a href="/site_music-download.xhtml">Music Category</a></li> <li><a href="/site_az-artist.xhtml">A-Z Indonesia</a></li> <li><a href="/site_download_lagu.xhtml">List Uploader</a></li>  </ul> </li> <li><a href="/site_feed.xhtml">SL Feed</a></li><li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><span class="">Contact</span> <span class="caret"></span></a> <ul class="dropdown-menu"><li><a href="http://fb.com/sahabatsharelagu" rel="nofollow" target="_blank">Facebook</a></li><li><a href="http://twitter.com/sharelagutrends" rel="nofollow" target="_blank">Twitter</a></li> <li><a href="/site_contact.xhtml" rel="nofollow">Contact Us</a></li>  <li><a href="#">SiteMap</a></li>  </ul> </li> </ul></div>
+</div> 
+</div><div id="navbar-height-col" style="left: -80%;"></div>
+<div class="header spacer"></div>
